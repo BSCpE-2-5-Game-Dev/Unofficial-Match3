9a052fa0466ffbe3032aaf60d21483c1cc4c022a
dilahK38 <72679649+dilahK38@users.noreply.github.com>
2021-01-12 06:01:43 +0800

    Update Board.lua

--- src/Board.lua
+++ src/Board.lua
@@ -258,7 +258,7 @@ function Board:getFallingTiles()
             if not tile then
 
                 -- new tile with random color and variety
-                local tile = Tile(x, y, math.random(18), math.random(6))
+                local tile = Tile(x, y, math.random(18), math.random(math.min(6, self.level)))
                 tile.y = -32
                 self.tiles[y][x] = tile
 
