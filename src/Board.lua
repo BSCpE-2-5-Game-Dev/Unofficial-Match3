0e7dd88314433ea826e8c3420d5ee19f8f28f89b
dilahK38 <72679649+dilahK38@users.noreply.github.com>
2021-01-12 06:00:41 +0800

    2nd update
    only flat blocks present at level 1 and generate other variety for preceding levels

--- src/Board.lua
+++ src/Board.lua
@@ -13,10 +13,12 @@
 
 Board = Class{}
 
-function Board:init(x, y)
+function Board:init(x, y, level)
     self.x = x
     self.y = y
     self.matches = {}
+    
+    self.level = level
 
     self:initializeTiles()
 end
@@ -32,7 +34,7 @@ function Board:initializeTiles()
         for tileX = 1, 8 do
             
             -- create a new tile at X,Y with a random color and variety
-            table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(18), math.random(6)))
+            table.insert(self.tiles[tileY], Tile(tileX, tileY, math.random(18), math.random(math.min(6, self.level))))
         end
     end
 
@@ -159,6 +161,22 @@ function Board:calculateMatches()
 
     -- return matches table if > 0, else just return false
     return #self.matches > 0 and self.matches or false
+end
+
+--[[ 
+    Gain more score each level based on variety present in
+]]
+function Board:calculateVariety()
+    local points = 0
+
+    for k, match in pairs(self.matches) do
+        for k, tile in pairs(match) do
+            if tile.variety > 1 then
+                points = tile.variety * 50 
+            end
+        end
+    end
+    return points
 end
 
 --[[
